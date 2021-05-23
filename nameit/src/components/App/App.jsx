a040204eed52fc8c31ea2860ef02d3a46d0f19cc
ShristiJalan <shristijalan1010@gmail.com>
2021-05-23 15:20:56 +0530

    styling header component

--- nameit/src/components/App/App.jsx
+++ nameit/src/components/App/App.jsx
@@ -1,10 +1,17 @@
 import React from 'react'; 
+import Header from '../Header/Header';
 import './App.css';
 
 class App extends React.Component {
 
     render() {
-        return <h1>This is my functional component!</h1>
+        return(
+            <div>
+                <Header>
+                    <Header />
+                </Header>
+            </div>
+        )
     }
 }
 
