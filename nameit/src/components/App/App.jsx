67b0d6f603a5d2643a4cd159075fcc621e0cd020
ShristiJalan <shristijalan1010@gmail.com>
2021-05-23 20:10:20 +0530

    Search Box Component

--- nameit/src/components/App/App.jsx
+++ nameit/src/components/App/App.jsx
@@ -1,15 +1,20 @@
 import React from 'react'; 
+import SearchBox from '../SearchBox/SearchBox'
 import Header from '../Header/Header';
 import './App.css';
 
+
 class App extends React.Component {
+
+    state = {
+        headerText: "Name It!",
+    };
 
     render() {
         return(
             <div>
-                <Header>
-                    <Header />
-                </Header>
+                    <Header headTitle = {this.state.headerText} />
+                    <SearchBox />
             </div>
         )
     }
