31f6192710368807a4e33f1d0fd88d099ca77273
ShristiJalan <shristijalan1010@gmail.com>
2021-06-19 12:36:10 +0530

    few changes in theme

--- nameit/src/components/Header/Header.jsx
+++ nameit/src/components/Header/Header.jsx
@@ -6,7 +6,7 @@ import './Header.css';
 const Header = ({ headTitle, headerExpanded }) => {
     return (
         <div className="head-container">
-            <img src="https://user-images.githubusercontent.com/26179770/106359099-693e3380-6336-11eb-8069-9e36f25de5ca.png"
+            <img src="https://user-images.githubusercontent.com/68010704/122605238-ce4fb700-d094-11eb-9594-d1b3f2b3eef0.png"
                 className={`head-image ${headerExpanded ? 'head-image-expanded' : 'head-image-contracted'}`} alt="image" />
 
             <h1 className="{`head-text ${headerExpanded ? 'head-text-expanded' : 'head-text-contracted'}`}">{headTitle}</h1>
